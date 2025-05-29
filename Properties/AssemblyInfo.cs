f2902791e664f6c663882babd21627c14ae3b2be
VisualSolucionesInf <administracion@visualsoluciones.net>
2025-05-29 16:52:49 +0200

    Solución inicial.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("QrGeneratorComLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QrGeneratorComLib")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3f75f1fd-cc3d-4adf-9b9d-6aacfa7f1543")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QrGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using QRCoder;
+
+namespace QrGeneratorComLib
+{
+    [ComVisible(true)]
+    [Guid("B1111111-2222-3333-4444-555555555555")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IQrGenerator
+    {
+        byte[] GenerateQrBytes(string text);
+        string GenerateQrBase64(string text);
+        void SaveQrToFile(string text, string filePath, int width, int height);
+    }
+
+    [ComVisible(true)]
+    [Guid("A1111111-2222-3333-4444-555555555555")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class QrGenerator : IQrGenerator
+    {
+        private Bitmap GenerateQrBitmap(string text, int? width = null, int? height = null)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var data = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(data);
+            Bitmap bmp = qrCode.GetGraphic(20); // scale factor 20
+
+            if (width.HasValue && height.HasValue)
+            {
+                return new Bitmap(bmp, new Size(width.Value, height.Value));
+            }
+
+            return bmp;
+        }
+
+        public byte[] GenerateQrBytes(string text)
+        {
+            using (var bmp = GenerateQrBitmap(text))
+            using (var ms = new MemoryStream())
+            {
+                bmp.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        public string GenerateQrBase64(string text)
+        {
+            using (var bmp = GenerateQrBitmap(text))
+            using (var ms = new MemoryStream())
+            {
+                bmp.Save(ms, ImageFormat.Png);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public void SaveQrToFile(string text, string filePath, int width, int height)
+        {
+            using (var bmp = GenerateQrBitmap(text, width, height))
+            {
+                bmp.Save(filePath, ImageFormat.Png);
+            }
+        }
+    }
+}
